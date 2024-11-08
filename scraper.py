c20b26255dd941c8297018acfc268676ae3186b0
Isaul <tiradolisaul@gmail.com>
2024-11-07 20:18:50 -0700

    get request function implemented: working

--- scraper.py
+++ scraper.py
@@ -1,10 +1,25 @@
 import requests
 
 
-def main():
-    pass
+def read_url() -> str:
+    url = input('Intput thr URL: \n')
+    return url
+
+
+def get_request(url: str, headers: dict = None):
+    return requests.get(url, headers=headers)
+
+
+def main() -> None:
+    url = 'https://icanhazdadjoke.com/j/LBAQ79MJmb'
+    request = get_request(url, {'Accept': 'application/json'})
+    if request.status_code == 200:
+        data = request.json()
+        print(data['joke'])
+    else:
+        print('Invalid resource!')
 
 
 if __name__ == '__main__':
-    print('requests version: ', requests.__version__)
+    # print('requests version: ', requests.__version__)
     main()
