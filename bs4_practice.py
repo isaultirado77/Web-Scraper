f2966ba92224d53cbe58fb701448da77249bfea6
Isaul <tiradolisaul@gmail.com>
2024-11-11 19:08:16 -0700

    reviewing page

--- bs4_practice.py
+++ bs4_practice.py
@@ -1,19 +1,4 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://www.nature.com/articles/d41586-022-04498-3')
-
-# Create a parse tree
-# r.content: data of the page
-# 'html.parser' python built-in parser
-soup = BeautifulSoup(r.content, 'html.parser')
-
-
-def main():
-    print('Status Request', r.status_code, end='\n')
-    title_tag = soup.find('title')
-    title = title_tag.get_text(strip=True) if title_tag else "No title found"
-    print(title)
-    description_tag = soup.find('meta', {'name': 'description'})
-    description = description_tag['content'].strip() if description_tag else "No description found"
-    print(description)
+response = requests.get('https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3')
